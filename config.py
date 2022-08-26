d7143b33a8b466e8bdf8051e8fc820d33a432e4d
unknown <gachaybo0909@gmail.com>
2022-08-26 09:36:22 +0700

    commit

---
+++ config.py
@@ -0,0 +1,29 @@
+page_modules = ['Page Posts']
+scrape_pages = True
+two_factor_authentication = True
+headless_mode = False
+
+
+
+min_func_calls = 5
+max_func_calls = 10
+
+rate_limiting = False
+
+# subject to change with time
+min_wait_delay = 80
+max_wait_delay = 110
+# ---------------------------------------------------------------
+
+proxy_host = ""  
+proxy_port = ""  
+proxy_user = ""  
+proxy_pass = ""  
+
+total_scrolls = 3
+
+browser = 'Chrome'
+
+email = "vudu15041997@gmail.com"
+password = "VanLuat259"
+key = "GI3FHHBXW4SLQNJ6JFROF5SSJ4LJRFSV"
